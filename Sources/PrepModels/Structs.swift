f3e5727a7f9425856a98489689b40cf674f9c1dc
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-21 05:55:14 +0500

    tag increment

--- Sources/PrepModels/Structs.swift
+++ Sources/PrepModels/Structs.swift
@@ -23,17 +23,35 @@ public struct Density: Codable {
     public var weightUnit: Int16
     public var volumeDouble: Double
     public var volumeUnit: Int16
+    
+    public init(weightDouble: Double, weightUnit: Int16, volumeDouble: Double, volumeUnit: Int16) {
+        self.weightDouble = weightDouble
+        self.weightUnit = weightUnit
+        self.volumeDouble = volumeDouble
+        self.volumeUnit = volumeUnit
+    }
 }
 
 public struct Barcode: Codable {
     public var payload: String
     public var symbology: Int16
+    
+    public init(payload: String, symbology: Int16) {
+        self.payload = payload
+        self.symbology = symbology
+    }
 }
 
 public struct Micronutrient: Codable {
     public var name: String
     public var amount: Double
     public var unit: Int16
+    
+    public init(name: String, amount: Double, unit: Int16) {
+        self.name = name
+        self.amount = amount
+        self.unit = unit
+    }
 }
 
 public struct Nutrients: Codable {
@@ -42,6 +60,14 @@ public struct Nutrients: Codable {
     public var protein: Double
     public var fat: Double
     public var micronutrients: [Micronutrient]
+    
+    public init(energyInKcal: Double, carb: Double, protein: Double, fat: Double, micronutrients: [Micronutrient]) {
+        self.energyInKcal = energyInKcal
+        self.carb = carb
+        self.protein = protein
+        self.fat = fat
+        self.micronutrients = micronutrients
+    }
 }
 
 public struct Size: Codable {
@@ -55,4 +81,16 @@ public struct Size: Codable {
     public var volumeUnit: Int16?
     public var sizeUnitId: UUID?
     public var sizeUnitVolumePrefixUnit: Int16?
+    
+    public init(name: String, volumePrefixUnit: Int16? = nil, quantity: Double, amount: Double, unit: Int16, weightUnit: Int16? = nil, volumeUnit: Int16? = nil, sizeUnitId: UUID? = nil, sizeUnitVolumePrefixUnit: Int16? = nil) {
+        self.name = name
+        self.volumePrefixUnit = volumePrefixUnit
+        self.quantity = quantity
+        self.amount = amount
+        self.unit = unit
+        self.weightUnit = weightUnit
+        self.volumeUnit = volumeUnit
+        self.sizeUnitId = sizeUnitId
+        self.sizeUnitVolumePrefixUnit = sizeUnitVolumePrefixUnit
+    }
 }
